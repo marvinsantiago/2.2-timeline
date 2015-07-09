5f8f8f8002d7127dfa259913c73c2cdcb105049e
Marvin Santiago <marvin.santiago@ymail.com>
2015-07-08 22:02:55 -0500

    Able to get the date on left of the line and but working on getting icon lined up on the line.

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,6 @@
 var merge = require('broccoli-merge-trees');
 var compileSass = require('broccoli-sass');
 
-var appCss = compileSass(['styles', 'bower_components'], 'style.scss', 'style.css');
+var appCss = compileSass(['assets', 'bower_components'], 'app.scss', 'app.css');
 
 module.exports = merge(['public', 'bower_components', appCss]);
